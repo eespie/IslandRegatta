18b18836c3c326da5fe2ee7636eaa54c6970bb62
Eric <eric.espie@eboreal.com>
2024-12-04 22:18:38 +0100

    maps

---
+++ scenes/tools/CameraController.gd
@@ -0,0 +1,59 @@
+extends Camera2D
+
+@export var zoom_speed : float = 10
+@export var zoom_min : float = 0.09
+
+@onready var tile_map_layer = %TileMapLayer
+@onready var marker_2d_min = %Marker2DMin
+@onready var marker_2d_max = %Marker2DMax
+
+var zoom_target :Vector2
+var dragStartMousePos = Vector2.ZERO
+var dragStartCameraPos = Vector2.ZERO
+var isDragging : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	zoom_target = zoom
+	set_cam_limits()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	Zoom(delta)
+	ClickAndDrag()
+	
+func Zoom(delta):
+	if Input.is_action_just_pressed("camera_zoom_in"):
+		zoom_target *= 1.1
+		set_cam_limits()
+		
+	if Input.is_action_just_pressed("camera_zoom_out"):
+		zoom_target *= 0.9
+		set_cam_limits()
+	
+	if zoom_target.x < zoom_min:
+		zoom_target = Vector2(zoom_min, zoom_min)
+	zoom = zoom.slerp(zoom_target, zoom_speed * delta)
+
+func set_cam_limits():
+	var vp_size : Vector2 = get_viewport().get_visible_rect().size
+		
+	limit_left = marker_2d_min.position.x + 2.0 * zoom_target.x / vp_size.x
+	limit_right = marker_2d_max.position.x - 2.0 * zoom_target.x / vp_size.x
+	limit_top = marker_2d_min.position.y + 2.0 * zoom_target.y / vp_size.y
+	limit_bottom = marker_2d_max.position.y - 2.0 * zoom_target.y / vp_size.y
+
+func ClickAndDrag():
+	if !isDragging and Input.is_action_just_pressed("camera_pan"):
+		dragStartMousePos = get_viewport().get_mouse_position()
+		dragStartCameraPos = position
+		isDragging = true
+		
+	if isDragging and Input.is_action_just_released("camera_pan"):
+		isDragging = false
+		
+	if isDragging:
+		var moveVector : Vector2 = get_viewport().get_mouse_position() - dragStartMousePos
+		position = dragStartCameraPos - moveVector * 1/zoom.x
+		
---
+++ scenes/tools/tile_map-generator.gd
@@ -0,0 +1,43 @@
+@tool
+extends Node
+
+@export var water_color : String
+@export var sand_color : String
+@export var grass_color : String
+@export var dirt_color : String
+
+@onready var terrain = %Terrain
+
+var tile_map : TileMapLayer
+@export var is_generated : bool = false
+
+func _process(_delta):
+	if is_generated:
+		return
+	
+	tile_map = get_parent()
+	var width : int = terrain.size.x
+	var height : int = terrain.size.y
+	var image : Image = terrain.get_texture().get_image()
+	var y : int = 0
+	var x : int = 0
+	
+	while y < height:
+		while x < width:
+			var color : Color = image.get_pixel(x, y)
+			match color.to_html():
+				water_color:
+					tile_map.set_cell(Vector2i(x,y), 0, Vector2i(0,0))
+				sand_color:
+					tile_map.set_cell(Vector2i(x,y), 0, Vector2i(1,0))
+				grass_color:
+					tile_map.set_cell(Vector2i(x,y), 0, Vector2i(2,0))
+				dirt_color:
+					tile_map.set_cell(Vector2i(x,y), 0, Vector2i(3,0))
+				_:
+					print(color.to_html())
+			x += 1
+		y += 1
+		x = 0		
+
+	is_generated = true
