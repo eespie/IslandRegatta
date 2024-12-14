49ba7c87135a21fae4f8491088470b19813d892a
Eric <eric.espie@eboreal.com>
2024-12-14 10:53:03 +0100

    prep for new layers

--- scenes/gameplay/wind.gd
+++ scenes/gameplay/wind.gd
@@ -1,8 +1,8 @@
 extends Node
 
 @export_category('Synoptic wind')
-@export_range(0, 360) var synoptic_wind_dir : int = 30
-@export_range(4, 35) var synoptic_wind_speed : int = 15
+@export_range(0, 360) var synoptic_wind_dir : int = 0
+@export_range(4, 35) var synoptic_wind_speed : int = 20
 
 @onready var map = %Map
 
